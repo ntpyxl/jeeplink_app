a5867400b3ae1f0ef3c3662f127ab4799d473286
ntpyxl <abayafrancis.e@gmail.com>
2026-01-20 16:55:38 +0800

    Separate API URL for local development and production

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-fetch("https://jeeplinkapi.vercel.app/")
+const API_URL =
+  location.origin !== "https://jeeplink-app.vercel.app"
+    ? "http://127.0.0.1:8000"
+    : "https://jeeplinkapi.vercel.app";
+
+fetch(API_URL + "/")
   .then(response => response.json())
   .then(data => {
     document.getElementById("textHere").textContent = data.Hello;
