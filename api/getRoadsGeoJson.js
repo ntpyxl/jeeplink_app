ea43b91d22705af5dad58ccae7abc9a62f3adae5
ntpyxl <abayafrancis.e@gmail.com>
2026-03-25 15:19:20 +0800

    Fixing caching on getting roads geojson data.

--- api/getRoadsGeoJson.js
+++ api/getRoadsGeoJson.js
@@ -18,6 +18,7 @@ export default async function handler(req) {
                 'CDN-Cache-Control': 'public, s-maxage=604800',
                 'Vercel-CDN-Cache-Control': 'public, s-maxage=604800',
                 'Cache-Control': 'public, s-maxage=604800, stale-while-revalidate=86400',
+                'x-vercel-protection-bypass': process.env.VERCEL_AUTOMATION_BYPASS_SECRET,
                 'Vary': 'Accept-Encoding', 
             },
         });
