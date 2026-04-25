bb0f9fb0821183ab066faf1eb03d35a80c80bdbe
ntpyxl <abayafrancis.e@gmail.com>
2026-04-25 21:15:47 +0800

    Added success and error notifications when updating a fare matrix

--- public/scripts/fareMatrixScript.js
+++ public/scripts/fareMatrixScript.js
@@ -199,16 +199,14 @@ $('#uploadFareMatrixFile').on('click', async () => {
             body: formData
         });
 
-        if (response.status == "Received") {
-            // TODO: Should add a modal that says upload is a success
-            console.log("Upload successful!");
-        }
-
         await loadFareMatrices();
         closeUpdateMatrixModal();
+        showSuccess("Successfully updated fare matrix table!")
+
         file = null;
     } catch (err) {
         console.error("Error:", err);
+        showError("Failed to update fare matrix table.");
     }
 });
 
