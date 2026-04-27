9be7806ab097c13b343495c76569e9f83d20ae31
Adi <randycornita22@gmail.com>
2026-04-27 16:36:31 +0800

    Added admin loading UI

--- public/scripts/core/accountHandler.js
+++ public/scripts/core/accountHandler.js
@@ -1,9 +1,17 @@
 import { apiFetch } from "./jeeplinkApiFetcher.js";
+import { adminShowLoader, adminHideLoader } from "../ui/adminStylingScript.js";
 
 export async function checkAuth() {
     let token = localStorage.getItem("token");
 
+    adminShowLoader([
+        "Loading...",
+        "Checking authentication...",
+        "Almost there..."
+    ]);
+
     if (!token) {
+        adminHideLoader();
         window.location.replace("./login.html");
         return;
     }
@@ -16,7 +24,7 @@ export async function checkAuth() {
             }
         });
 
-        $("body").css("visibility", "visible");
+        adminHideLoader();
         return {"loggedInUsername": response.username};
     } catch (error) {
         localStorage.removeItem("token");
